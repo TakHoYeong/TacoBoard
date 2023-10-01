acda361995af1ed1aa088420e892b82de5b2258b
TakHoYeong <xkrghdud123@naver.com>
2023-10-01 19:41:16 +0900

    add domain Article

---
+++ src/main/java/com/taco/tacoboard/domain/Article.java
@@ -0,0 +1,14 @@
+package com.taco.tacoboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}

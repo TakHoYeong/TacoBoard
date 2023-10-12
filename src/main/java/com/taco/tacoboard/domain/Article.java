cab2d859c4322073a35df8e33e1fbb7a9e879eb8
TakHoYeong <xkrghdud123@naver.com>
2023-10-12 22:36:29 +0900

    domain set

--- src/main/java/com/taco/tacoboard/domain/Article.java
+++ src/main/java/com/taco/tacoboard/domain/Article.java
@@ -27,14 +27,21 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @Column(nullable = false) private String title; // 제목
-    @Setter @Column(nullable = false) private String content; // 본문
-    @Setter private String hashtag; // 해시태그
+    @Setter @Column(nullable = false)
+    private String title; // 제목
+    @Setter @Column(nullable = false)
+    private String content; // 본문
+    @Setter
+    private String hashtag; // 해시태그
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일자
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+    @CreatedDate @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일자
+    @CreatedBy @Column(nullable = false, length = 100)
+    private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일자
+    @LastModifiedBy @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
 
     protected Article() {}
 

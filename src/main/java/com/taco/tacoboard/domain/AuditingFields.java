3405f8aacadb60f8c3fbc7bef5a624eba8ca7d83
TakHoYeong <xkrghdud123@naver.com>
2023-10-30 22:39:28 +0900

    [Add] AuditingFields.java

---
+++ src/main/java/com/taco/tacoboard/domain/AuditingFields.java
@@ -0,0 +1,27 @@
+package com.taco.tacoboard.domain;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AuditingFields {
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일자
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일자
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
+}

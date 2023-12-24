5cb9b03fda73cfc829acdb20a7364126c5719d1c
TakHoYeong <xkrghdud123@naver.com>
2023-12-24 14:27:34 +0900

    [Modify] AuditingFields

--- src/main/java/com/taco/tacoboard/domain/AuditingFields.java
+++ src/main/java/com/taco/tacoboard/domain/AuditingFields.java
@@ -15,12 +15,15 @@ public class AuditingFields {
     @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일자
+
     @CreatedBy
     @Column(nullable = false, length = 100)
     private String createdBy; // 생성자
+
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일자
+
     @LastModifiedBy
     @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자

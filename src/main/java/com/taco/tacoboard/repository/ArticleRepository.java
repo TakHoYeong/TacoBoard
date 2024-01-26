3887b3640c9060175be1afa2c5a30fb535d42847
TakHoYeong <xkrghdud123@naver.com>
2024-01-26 17:42:14 +0900

    [Add] @RepositoryRestResource to ArticleRepo

--- src/main/java/com/taco/tacoboard/repository/ArticleRepository.java
+++ src/main/java/com/taco/tacoboard/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.taco.tacoboard.repository;
 
 import com.taco.tacoboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }

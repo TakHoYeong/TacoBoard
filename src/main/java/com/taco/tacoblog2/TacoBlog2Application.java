04f9d991afeb1b6fb3ba20cfec3a8e6f70b7ebb3
TakHoYeong <xkrghdud123@naver.com>
2024-04-04 19:07:39 +0900

    first commit

---
+++ src/main/java/com/taco/tacoblog2/TacoBlog2Application.java
@@ -0,0 +1,13 @@
+package com.taco.tacoblog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoBlog2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoBlog2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/taco/tacoblog2/TacoBlog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taco.tacoblog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoBlog2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

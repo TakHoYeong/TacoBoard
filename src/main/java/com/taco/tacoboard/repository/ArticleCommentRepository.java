01303283e5c62d4474e738bf35c7716670d22d54
TakHoYeong <xkrghdud123@naver.com>
2024-05-02 14:41:14 +0900

    [Add] repositories update

--- src/main/java/com/taco/tacoboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/taco/tacoboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,12 @@
 package com.taco.tacoboard.repository;
 
+import com.taco.tacoboard.domain.Article;
 import com.taco.tacoboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment> {
 }
--- src/main/java/com/taco/tacoboard/repository/ArticleRepository.java
+++ src/main/java/com/taco/tacoboard/repository/ArticleRepository.java
@@ -1,10 +1,17 @@
 package com.taco.tacoboard.repository;
 
 import com.taco.tacoboard.domain.Article;
+import com.taco.tacoboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+
 
 }
